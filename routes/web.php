1a39ec363505401c9346b4bcd2a3bec6306e5e9a
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2020-05-05 12:51:37 -0300

    #4 Curso de Laravel 5.3 - Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,32 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/nome/nome2/nome3', function() {
+    return 'Big route';
+})->name('big.route');
+
+Route::get('/big', function() {
+    return redirect()->route('big.route');
+});
+
+Route::any('/any', function() {
+    return 'Route any';
+});
+
+Route::match(['get', 'post'], '/match', function() {
+    return 'Route match';
+});
+
+Route::post('/post', function() {
+    return 'Route post';
+});
+
+Route::get('/contato', function() {
+    return 'contato';
+});
+
+Route::get('/empresa', function(){
+	return view('empresa');
+});
 
 Route::get('/', function () {
     return view('welcome');
