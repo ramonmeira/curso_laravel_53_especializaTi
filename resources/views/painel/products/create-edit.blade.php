d7ee5dfee4e4494df9a42716c13ffa65977a0f49
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2020-05-12 11:44:19 -0300

    #28 Curso de Laravel 5.3 - Pacote Collective Form and HTML

--- resources/views/painel/products/create-edit.blade.php
+++ resources/views/painel/products/create-edit.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
 
-<h1 class="title-pg">Gestão produto</h1>
+<h1 class="title-pg">
+	<a href="{{ route('product.index') }}">
+		<span class="glyphicon glyphicon-fast-backward"></span>
+		Voltar
+	</a>
+	Gestão Produto: <b>{{$product->name or 'Novo'}}</b>
+</h1>
 
 @if(isset($errors) && count($errors)>0)
 	<div class="alert alert-danger">
@@ -14,44 +20,29 @@
 @if (isset($product))
 <form class="form" method="post" action="{{route('product.update',$product->id)}}">
 	{!! method_field('PUT') !!}
+{!! Form::model($product,['route' => ['product.update', $product->id], 'class' => 'form', 'method' => 'put']) !!}
 @else
-<form class="form" method="post" action="{{route('product.store')}}">
+{!! Form::open(['route' => 'product.store', 'class' => 'form']) !!}
 @endif
-	<!--
-	<input type="hidden" name="_token" value="{{csrf_token()}}">
-	-->
-	{!! csrf_field() !!}
 	<div class="form-group">
-		<input type="text" name="name" placeholder="Nome:" class="form-control" value="{{$product->name or old('name')}}">
+		{!! Form::text('name',null,['class'=>'form-control','placeholder'=>'Nome:']) !!}
 	</div>
 	<div class="form-group">
 		<label>
-			<input type="checkbox" name="active" value="1" 
-			@if (isset($product) && $product->active=='1')
-				checked
-			@endif>
+			{!! Form::checkbox('active', ) !!}
 			Ativo?
 		</label>
 	</div>
 	<div class="form-group">
-		<input type="text" name="number" placeholder="Número:" class="form-control"  value="{{$product->number or old('number')}}">
+		{!! Form::text('number',null,['class'=>'form-control','placeholder'=>'Número:']) !!}
 	</div>
 	<div class="form-group">
-		<select name="category" class="form-control">
-			<option value="">Escolha a Categoria</option>
-			@foreach($categorys as $category)
-			<option value="{{$category}}"
-			@if (isset($product) && $product->category==$category)
-				selected 
-			@endif
-			>{{$category}}</option>	
-			@endforeach
-		</select>
+		{!! Form::select('category',$categorys,null,['class'=>'form-control']) !!}
 	</div>
 	<div class="form-group">
-		<textarea name="description" placeholder="Descrição" class="form-control">{{$product->description or old('description')}}</textarea>
+		{!! Form::textarea('description',null,['class'=>'form-control','placeholder'=>'Descrição']) !!}
 	</div>
 
-	<button class="btn btn-primary">Enviar</button>
-</form>
+	{!! Form::submit('Enviar',['class'=>'btn btn-primary']) !!}
+{!! Form::close() !!}
 @endsection
